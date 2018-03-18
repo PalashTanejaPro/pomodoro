1f4e73033fe9e15872016f6c5bb80bc1fcc53dcb
Palash Taneja <tanejapalash@gmail.com>
2018-03-18 21:06:59 +0530

    db

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import time
+from tinydb import TinyDB
 import os
 import wikiquote
 import random
+from datetime import datetime
+
+db = TinyDB('db.json')
 
 def beep():
     print ('\a')
@@ -12,8 +16,12 @@ def beep():
 
 for i in range(5):
     os.system("clear")
-    t = input("Time of the session:")
+    t = input("Time of the session: ")
+    start = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     time.sleep(int(t)*60)
+    end = datetime.now().strftime('%Y-%m-%d %H:%M:%S')   
     beep()
     print (random.choice(wikiquote.quotes('Work', lang='en'))) 
+    log = input("What did you do in this session? ")
+    db.insert({'start': str(start), 'end': str(end), 'log': log})
 
